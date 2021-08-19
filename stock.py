3b2324d7e6351177cc0dbdac7088158c732f8ca2
Peter Frost <frosticless@hotmail.com>
2021-08-19 21:37:26 +0100

    Refactor, add comparison to previous status to avoid repeat messages

--- stock.py
+++ stock.py
@@ -9,14 +9,22 @@ import discord
 from discord.ext import tasks 
 from shutil import which
 
-def signal_handler(sig, frame):
+prevProducts = {}
+
+#
+# User Interface stuff
+#
+def signal_handler():
     print('\033[?1049l')
     sys.exit(0)
-
 signal.signal(signal.SIGINT, signal_handler)
 print('\033[?1049h')
 
 
+
+#
+# Discord stuff
+#
 TOKEN = open("token.txt","r").readline()
 client = discord.Client()
 start = time.time()
@@ -28,10 +36,18 @@ async def on_ready():
     channel = client.get_channel(877946391647903876)
     await channel.send("Hello!")
 
-@tasks.loop(seconds=10)
+@tasks.loop(seconds = 10)
 async def loop_task():
-    channel = client.get_channel(877946391647903876)
+    await check_stock()
 
+
+
+#
+# Stock checking stuff
+#
+async def check_stock():
+    global prevProducts
+    channel = client.get_channel(877946391647903876)
     print('\033[2J\033[3J\033[1;1HLoading...')
 
     # using curl seems to get blocked less often
@@ -55,22 +71,26 @@ async def loop_task():
     featured = jsonDict["searchedProducts"]["featuredProduct"]
     products = jsonDict["searchedProducts"]["productDetails"]
     products.append(featured)
-    #print(json.dumps(products, indent=4, sort_keys=True))
-
     print('\033[1;1H\033[2K')
-    for product in products:
+
+    for i, product in enumerate(products):
         print(product["displayName"], end=' ')
+
         if product["prdStatus"] == 'out_of_stock':
             print('\033[31m' + product["prdStatus"] + '\033[0m', end=' ')
         else:
             print('\033[32;5m' + product["prdStatus"] + '\033[0m', end=' ')
-            await channel.send(product["displayName"] + ' ' + 
-                                product["prdStatus"] + ' ' + 
-                                product["retailers"][0]["purchaseLink"])
+
+            if prevProducts != {} and product != prevProducts[i]:
+                await channel.send(product["displayName"] + ' ' + 
+                                    product["prdStatus"] + ' ' + 
+                                    product["retailers"][0]["purchaseLink"])
+
         print(product["retailers"][0]["purchaseLink"])
 
+    prevProducts = products
     randTime = round(10 + random.uniform(0, 10))
-    loop_task.change_interval(seconds=randTime)
+    loop_task.change_interval(seconds = randTime)
     print('\033[1G\033[2K' + f"{randTime}", end=' ', flush=True)
 
 
